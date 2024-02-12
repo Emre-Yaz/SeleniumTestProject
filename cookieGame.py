50a76f965b15592a053c169eefa3bb162760bce0
Emre-Yaz <ibrahimemreyaz@gmail.com>
2024-02-13 02:07:07 +0300

    Created a bot for the cookie game.

--- cookieGame.py
+++ cookieGame.py
@@ -13,22 +13,45 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 TIMES_TO_CLICK = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
 
 # Functions
+# Constantly clicking the cookie
+
+
 def click_cookie(driver):
     cookie = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "bigCookie")))
     actions = ActionChains(driver)
     actions.click(cookie)
     actions.perform()
 
+# Extract the price as integer
 
-def purchase_items(driver, count):
+
+def get_product_price(driver, product_id):
+    price_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, product_id)))
+    price_text = price_element.text
+    if 'million' in price_text:
+        price = float(price_text.split(" ")[0].replace(',', '')) * 1000000
+    elif 'billion' in price_text:
+        price = float(price_text.split(" ")[0].replace(',', '')) * 1000000000
+    else:
+        price = float(price_text.split(" ")[0].replace(',', ''))
+    return price
+
+# Buy upgrades when possible
+
+
+def purchase_items(driver, budget):
     for i in range(1, -1, -1):
-        item = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "productPrice" + str(i))))
-        value = int(item.text)
-        if value <= count:
+        product_id = "productPrice" + str(i)
+        price = get_product_price(driver, product_id)
+        if price <= budget:
+            item = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, product_id)))
             upgrade_actions = ActionChains(driver)
             upgrade_actions.move_to_element(item)
             upgrade_actions.click()
             upgrade_actions.perform()
+
+
+# Extract the cookie count as integer
 
 
 def get_cookie_count(driver):
@@ -42,6 +65,7 @@ def get_cookie_count(driver):
         count = int(cookie_count_text.replace(',', '').split(" ")[0])
     return count
 
+
 try:
     # Load webpage
     driver.get("https://orteil.dashnet.org/cookieclicker/")
@@ -52,6 +76,7 @@ try:
     actions.click(lang_select)
     actions.perform()
 
+    # Play the game
     for _ in range(TIMES_TO_CLICK):
         click_cookie(driver)
         count = get_cookie_count(driver)
